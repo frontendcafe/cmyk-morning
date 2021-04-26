9b1987a857b59f03f7b9be5e8a8a99aeeb4ed31b
Carolina Mamani <mamaniccarolina@gmail.com>
2021-04-26 14:55:22 -0300

    number of enrolled participants handler added

--- src/form.js
+++ src/form.js
@@ -1,6 +1,7 @@
 const db = firebase.firestore();
 const $form = document.form;
 const $sendBtn = document.querySelector('#send-form');
+const increment = firebase.firestore.FieldValue.increment(1);
 let sport = 'cycling-individual';
 
 const addParticipant = (sport, name, phone) => {
@@ -8,6 +9,7 @@ const addParticipant = (sport, name, phone) => {
     name,
     phone,
   });
+  db.collection('sports').doc(`${sport}`).update({ enrolled: increment });
 };
 
 $form.addEventListener('submit', async (e) => {
