6d3d77dc2d7362a63ef930bc712f6ced2ad8c544
YoimelDev <yoimelito@gmail.com>
2021-04-28 21:30:45 -0400

    mobilemenu.js added, animations added to header

--- src/app.js
+++ src/app.js
@@ -69,25 +69,3 @@ document.querySelector(".buttonLeft").addEventListener("click", () => {
     $cards.scrollLeft -= 317 * 5;
   }
 });
-
-// Menu Responsive
-document.addEventListener('DOMContentLoaded', function() {
-
-  eventListeners();
-});
-
-function eventListeners() {
-  const mobileMenu = document.querySelector('.nav__mobileMenu');
-
-  mobileMenu.addEventListener('click', navegacionResponsive);
-}
-
-function navegacionResponsive() {
-  const navegacion = document.querySelector('.nav__link');
-
-  if (navegacion.classList.contains('show')) {
-    navegacion.classList.remove('show');
-  } else {
-    navegacion.classList.add('show');
-  }
-}
